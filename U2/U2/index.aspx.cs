b997e777cd933e8ea3b199b05c709cda9de97ba3
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-09-18 09:43:14 +0700

    kết nối CSDL SQL Server

---
+++ U2/U2/index.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var db = new EmployeeDbEntities();
+        var employees = db.Employees;
+        var listEmployee = employees.ToList();
+        var s = string.Empty;
+        foreach(Employee emp in listEmployee) { s += emp.EmployeeName + " "; };
+        lblListEmployee.Text = s;
+
+    }
+}
